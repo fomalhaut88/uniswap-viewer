79a085d68791e879f6e841624ea07ad05e603aca
Alexander X <alexfomalhaut@gmail.com>
2025-05-05 22:07:29 +0300

    Add CI/CD.

--- setup.py
+++ setup.py
@@ -10,8 +10,10 @@ setup(
     name='uniswap-viewer',
     version="0.1.0",
     packages=['uniswap_viewer'],
+    author="Alexander Fomalhaut",
+    url="https://github.com/fomalhaut88/uniswap-viewer",
     license="MIT",
-    description="",
+    description="Lightweight Python library for reading Uniswap V3 prices and tick data via Web3.",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     install_requires=[
@@ -23,4 +25,19 @@ setup(
     package_data={
         'uniswap_viewer': ['source/*.json'],
     },
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    keywords="uniswap v3 web3 defi ethereum blockchain async",
 )
